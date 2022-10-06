529596a62fb820e187abcd736b62d8c1183e2824
WEI CHEN <42794651+wc253@users.noreply.github.com>
2022-10-06 22:11:54 +0800

    Add files via upload

---
+++ fig_diff_lp/DL.m
@@ -0,0 +1,27 @@
+function [B_est, H_est,T_dl]= DL(Y,SNR,i)
+
+eta = @(r,lam) sign(r).*max(bsxfun(@minus,abs(r),lam),0); 
+load(['listapara',num2str(i),'.mat']);
+% load(['netpara.mat']);
+
+[M,N] = size(B_0);
+erre=10^(-SNR/10);
+
+tic
+By=B_0*Y;
+xhat=eta(By,lam_0);
+
+for t=1:19
+    lam=eval(['lam_',num2str(t)]);
+    xhat=eta( S_0*xhat+By,lam);
+end
+
+H=xhat(1:M/2)+1i*xhat(M/2+1:M);
+[H_sort,pos]=sort(sum(abs(H).^2,2),'descend');
+B_est =pos(H_sort>erre);
+H_est=zeros(M/2,1);
+H_est(B_est,:)=H(B_est,:);
+toc
+T_dl=toc;
+
+
---
+++ fig_diff_lp/DNET.m
@@ -0,0 +1,30 @@
+function [B_est, H_est,T_dnet] = DNET(Y,Yd,SNR,i)
+
+load(['dnetpara',num2str(i),'.mat']);
+% load(['netpara.mat']);
+
+[M,N] = size(B_0);
+
+erre=10^(-SNR/10);
+
+tic
+By=B_0*Y;
+By1=B1_0'*Yd;
+xcombin=[By,By1].*max(1-lam_0./sqrt(sum(abs([By,By1]).^2,2)),0);
+xhat=xcombin(:,1);
+xhat1=xcombin(:,2:end);
+for t=1:19
+    lam=eval(['lam_',num2str(t)]);
+    xcombin=[S_0*xhat+By,S1_0'*xhat1+By1].*max(1-lam./sqrt(sum(abs([S_0*xhat+By,S1_0'*xhat1+By1]).^2,2)),0);
+    xhat=xcombin(:,1);
+    xhat1=xcombin(:,2:end);  
+end
+
+H=xhat(1:M/2)+1i*xhat(M/2+1:M);
+[H_sort,pos]=sort(sum(abs(H).^2,2),'descend');
+B_est =pos(H_sort>erre);
+H_est=zeros(M/2,1);
+H_est(B_est,:)=H(B_est,:);
+toc
+T_dnet=toc;
+
